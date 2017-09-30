8fae3513491bafcefc22487774c9d74636f0e543
koko <koko.zhang@finisar.com>
2017-09-30 10:29:15 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace observer2 {
+  class Program {
+    static void Main( string[ ] args ) {
+
+      Subject subject = new ConcreteSubject( );
+      Observer obs = new Observer( "Lily" );
+      Observer obs1 = new Observer( "May" );
+      subject.addObserver( obs );
+      subject.addObserver( obs1 );
+      subject.doSomething( );
+
+      Console.ReadLine( );
+
+    }
+  }
+}
---
+++ Subject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace observer2 {
+  public abstract class Subject {
+    private List<Observer> obsList = new List<Observer>( );
+
+    public void addObserver(Observer ob ) {
+      obsList.Add( ob );
+    }
+
+    public void delObserver(Observer ob ) {
+      obsList.Remove( ob );
+    }
+
+    public void NotifyObserver( ) {
+      foreach( var o in obsList ) {
+        o.Update( );
+      }
+    }
+
+    public abstract void doSomething( );
+  }
+
+  public class ConcreteSubject :Subject{
+    public override void doSomething( ) {
+      Console.WriteLine( "ConcreteSubject doing..." );
+      base.NotifyObserver( );
+    }
+
+  }
+
+
+  // --------------------- 观察者
+  public class Observer {
+    public string Name { get; set; }
+    public Observer(string name ) {
+      Name = name;
+    }
+    public void Update( ) {
+      Console.WriteLine( $"{Name} Observer update..." );
+    }
+  }
+}
